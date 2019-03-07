3b169f1dbb586f21b96b1dfc35db0f30f527bc48
cloner1984 <48311024+cloner1984@users.noreply.github.com>
2019-03-07 01:24:32 -0800

    Update __init__.py

---
+++ applications/arts/__init__.py
@@ -0,0 +1,48 @@
+"name": "php-di/demo",
+    "license": "MIT",
+    "type": "project",
+    "autoload": {
+        "psr-4": {
+            "SuperBlog\\": "src/SuperBlog/"
+        }
+    },
+    "require": {
+        "php": ">=7.0",
+        "php-di/php-di": "^6.0",
+        "nikic/fast-route": "~0.5.0",
+        "twig/twig": "^1.18",
+      "mnapoli/silly": "^1.7"
+        include/rcp
+    exclude/bts/codec/rgb/trs/hsp
+    include/192/exclude/168/gts/pts/hps/lion/dir/tbs/mion/include/900/wsd/b16000/900/tbs/5000/train/
+    192.168.1.15
+    include/query/include/\source/rgb/login/bts/cloud/include/199/380/\exclude/ramp/+90/bts/600/tps
+    {
+    "name": "php-di/demo",
+    "license": "MIT",
+    "type": "project",
+    "autoload": {
+        "psr-4": {
+            "SuperBlog\\": "src/SuperBlog/"
+        }
+    },
+    "require": {
+        "php": ">=7.0",
+        "php-di/php-di": "^6.0",
+        "nikic/fast-route": "~0.5.0",
+        "twig/twig": "^1.18",
+      "mnapoli/silly": "^1.7"
+        include/rcp/+905380785338/incoming/call-text/192.168.1.104
+    
+        exclude/rcp/+905380785338/outgoing/call-text/192.168.1.104
+        reply/\https://github.com/shogun-toolbox/shogun/edit/develop/applications/+905380785338/__init__.py/bireysel.turktelecom.com.tr/bluestacks/googleplay/app
+        
+</title><link rel="shortcut icon" type="image/x-icon" href="/mobil/+905380785338/favicon.ico" />
+<script type="text/javascript">
+//<![CDATA[
+var theForm = document.forms['\platform/+905380785338\income/outgo\reply/ping\192.168.1.104'];
+if (!theForm) {
+    theForm = document.aspnetForm;
+    }
+}
+
